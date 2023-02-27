11f3f6911e540fbee5ebfdbae8b8bf6b605e6b01
ElenaSayfullina <elena-fairy@yandex.ru>
2023-02-27 16:47:09 +0400

    HW01

--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -1,8 +1,10 @@
 package ru.javaops.masterjava.matrix;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
 /**
  * gkislin
@@ -15,23 +17,86 @@ public class MatrixUtil {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
+        final int taskCount = 10;
+
+        class Task implements Runnable {
+            final int startI;
+            final int finishI;
+
+            public Task(int startI, int finishI) {
+                this.startI = startI;
+                this.finishI = finishI;
+            }
+
+            public void run() {
+                int[] thatColumn = new int[matrixSize];
+
+                for (int j = 0; j<matrixSize; j++) {
+                    for (int k = 0; k < matrixSize; k++) {
+                        thatColumn[k] = matrixB[k][j];
+                    }
+
+                    for (int i = startI; i < finishI; i++) {
+                        int[] thisRow = matrixA[i];
+                        int sum = 0;
+                        for (int k = 0; k < matrixSize; k++) {
+                            sum += thisRow[k] * thatColumn[k];
+                        }
+                        matrixC[i][j] = sum;
+                    }
+                }
+            }
+        }
+
+        List<Task> tasks = new ArrayList<Task>();
+
+        final int cellsPerTask = matrixSize/taskCount;
+
+        for (int i = 1; i <= matrixSize; i++) {
+            if (i%cellsPerTask == 0) {
+                Task task = new Task(i - cellsPerTask, i);
+                tasks.add(task);
+            }
+        }
+
+        final CompletionService completionService = new ExecutorCompletionService<>(executor);
+        List<Future> futures = tasks.stream().map(task -> completionService.submit(task, null)).collect(Collectors.toList());
+
+        while (!futures.isEmpty()) {
+            Future future = completionService.poll(10, TimeUnit.SECONDS);
+            if (future == null) {
+                throw new InterruptedException();
+            }
+            futures.remove(future);
+            future.get();
+        }
+
         return matrixC;
     }
 
-    // TODO optimize by https://habrahabr.ru/post/114797/
     public static int[][] singleThreadMultiply(int[][] matrixA, int[][] matrixB) {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
-        for (int i = 0; i < matrixSize; i++) {
-            for (int j = 0; j < matrixSize; j++) {
-                int sum = 0;
+        int[] thatColumn = new int[matrixSize];
+
+        try {
+            for (int j = 0; ; j++) {
                 for (int k = 0; k < matrixSize; k++) {
-                    sum += matrixA[i][k] * matrixB[k][j];
+                    thatColumn[k] = matrixB[k][j];
                 }
-                matrixC[i][j] = sum;
+
+                for (int i = 0; i < matrixSize; i++) {
+                    int[] thisRow = matrixA[i];
+                    int sum = 0;
+                    for (int k = 0; k < matrixSize; k++) {
+                        sum += thisRow[k] * thatColumn[k];
+                    }
+                    matrixC[i][j] = sum;
+                }
             }
-        }
+        } catch (IndexOutOfBoundsException ignored) { }
+
         return matrixC;
     }
 
